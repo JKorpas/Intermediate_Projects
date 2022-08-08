bb47c11c742f05f26347fcd87c8c83d5e35ce051
Jakub <korpasjakub@gmail.com>
2022-08-08 13:32:37 +0200

    Quiz Game with own question data

---
+++ Quiz_Game/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+'''
+for index, question in enumerate(question_data):
+    index = Question(question["text"], question["answer"])
+    question_bank.append(index)'''
+
+question_bank = [Question(question["text"], question["answer"]) for question in question_data]
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score is {quiz.score}/{len(quiz.question_list)}")
