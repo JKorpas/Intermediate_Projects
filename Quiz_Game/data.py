ba6b979f7f6ae82399b176e8dc59c73e2623cdc9
Jakub <korpasjakub@gmail.com>
2022-10-03 17:13:06 +0200

    Clearing repo

--- Quiz_Game/data.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean")
-if response.ok == True:
-    question_data = response.json()["results"]
--- Quiz_Game/question_model.py
+++
@@ -1,6 +0,0 @@
-
-class Question:
-    def __init__(self, category, question, correct_answer):
-        self.category = category
-        self.text = question
-        self.answer = correct_answer
